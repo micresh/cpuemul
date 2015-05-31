b0050c3d8c11b04cd3f4ac6ff05bb2033015075e
Mikhail Reshetnyak <micresh@gmail.com>
2015-05-31 17:18:41 +0300

    modified:   cpu.v 	modified:   cpu_test.v adding any changes

--- cpu.v
+++ cpu.v
@@ -2,10 +2,15 @@ module ALU (A,B,S, opertype)
 
 	input [7:0] A;
 	input [7:0] B;
-	input [2:0] opertype;
+	input [3:0] opertype;
 	
 	output [7:0] S;
 	
-	assign S = A+B;
-	
+	always@(opertype)
+		begin
+			case(s)
+				4'b0000: S = A + B;
+				4'b0001: S = A - B;
+			endcase
+		end
 endmodule
---
+++ cpu_test.v
@@ -0,0 +1,6 @@
+module ALU_tb;
+	
+	reg [7:0] A = 8'b00000000;
+	reg [7:0]
+
+endmodule;
